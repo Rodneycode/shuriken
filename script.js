d58a7660c55382883c14ed8ba30f55f61892470d
FelipeNadhel <47465864+FelipeNadhelBR@users.noreply.github.com>
2020-07-16 21:26:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,15 @@ startTime();
 function goBack() {
   window.history.back();
 }
+
+function randomText() {
+    var randomtxt = [
+        'O jogo baixou e não tá aparecendo? Vá para a pasta onde onde os jogos ficam e extraia o .zip do jogo.',
+        'Não precisa ter paniko, se apenas aparecerem só botões, é sinal de que você não tem jogos.',
+        'É um desenvolvedor? <a href="https://discord.com/invite/HDgJyED">Envie seu jogo pelo nosso discord!</a>',
+        'Você pode ver artes da comunidade na aba de artworks.',
+        'Sample Text'];
+    return randomtxt[Math.floor((Math.random() * 3.99))];
+}
+
+document.getElementById("random").innerHTML = randomtext();
